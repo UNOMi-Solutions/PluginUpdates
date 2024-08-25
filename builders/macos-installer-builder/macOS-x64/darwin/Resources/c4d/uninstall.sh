f2a9574c5f332556869d10093510a187a480fbbe
Arsenic Bismuth <d.faris323@gmail.com>
2024-08-25 10:55:29 +0700

    Mac installer - Uninstaller script
    - Remove plugins from Cinema 4D apps

--- builders/macos-installer-builder/macOS-x64/darwin/Resources/c4d/uninstall.sh
+++ builders/macos-installer-builder/macOS-x64/darwin/Resources/c4d/uninstall.sh
@@ -7,6 +7,16 @@ DATE=`date +%Y-%m-%d`
 TIME=`date +%H:%M:%S`
 LOG_PREFIX="[$DATE $TIME]"
 
+PRODUCT_HOME=/Library/__ROOT_DIR__/__APP_ID__
+PLUGIN_DIR=__PLUGIN_DIR__
+APP_URL="org.__ORG__.__APP_ID__"
+APP_DIRS=("/Applications/Maxon Cinema 4D R25"
+"/Applications/Maxon Cinema 4D S26"
+"/Applications/Maxon Cinema 4D 2023"
+"/Applications/Maxon Cinema 4D 2024"
+"/Applications/Maxon Cinema 4D 2025")
+
+
 #Functions
 log_info() {
     echo "${LOG_PREFIX}[INFO]" $1
@@ -46,27 +56,40 @@ echo "Application uninstalling process started"
 find "/usr/local/bin/" -name "__PRODUCT__-__VERSION__" | xargs rm
 if [ $? -eq 0 ]
 then
-  echo "[1/3] [DONE] Successfully deleted shortcut links"
+  echo "[1/4] [DONE] Successfully deleted shortcut links"
 else
-  echo "[1/3] [ERROR] Could not delete shortcut links" >&2
+  echo "[1/4] [ERROR] Could not delete shortcut links" >&2
 fi
 
 #forget from pkgutil
-pkgutil --forget "org.$PRODUCT.$VERSION" > /dev/null 2>&1
+pkgutil --forget "$APP_URL" > /dev/null 2>&1
 if [ $? -eq 0 ]
 then
-  echo "[2/3] [DONE] Successfully deleted application informations"
+  echo "[2/4] [DONE] Successfully deleted application informations"
 else
-  echo "[2/3] [ERROR] Could not delete application informations" >&2
+  echo "[2/4] [ERROR] Could not delete application informations" >&2
 fi
 
 #remove application source distribution
-[ -e "/Library/${PRODUCT}/${VERSION}" ] && rm -rf "/Library/${PRODUCT}/${VERSION}"
+[ -e "${PRODUCT_HOME}" ] && rm -rf "${PRODUCT_HOME}"
 if [ $? -eq 0 ]
 then
-  echo "[3/3] [DONE] Successfully deleted application"
+  echo "[3/4] [DONE] Successfully deleted application"
 else
-  echo "[3/3] [ERROR] Could not delete application" >&2
+  echo "[3/4] [ERROR] Could not delete application" >&2
+fi
+
+#remove from plugin directories
+for dir in "${APP_DIRS[@]}"
+do
+  full_dir="${dir}/plugins/${PLUGIN_DIR}"
+  [ -e "${full_dir}" ] && echo "Deleting ${full_dir}..." && rm -rf "${full_dir}"
+done
+if [ $? -eq 0 ]
+then
+  echo "[4/4] [DONE] Successfully deleted plugin directories"
+else
+  echo "[4/4] [ERROR] Could not delete plugin directories" >&2
 fi
 
 echo "Application uninstall process finished"
